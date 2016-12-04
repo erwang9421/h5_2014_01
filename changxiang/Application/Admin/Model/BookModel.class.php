7f0979a1e57d62289e267ab1d9a9479053c0cf23
liukaixin123 <3081766044@qq.com>
2016-12-04 09:39:13 +0800

    书评管理

--- changxiang/Application/Admin/Model/BookModel.class.php
+++ changxiang/Application/Admin/Model/BookModel.class.php
@@ -1,9 +1,7 @@
 <?php
-
 namespace Admin\Model;
 use Think\Model;
-
-class BookCommentModel extends Model{
+class BooksModel extends Model{
     public $_validate =array(
         //添加图书
         array("bookname","require","图书名称不能为空"),
