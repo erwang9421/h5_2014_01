022d5352e9428bcc58b299ca559c6a6a227fc2e0
liukaixin123 <3081766044@qq.com>
2016-12-06 10:32:40 +0800

    评论管理

--- changxiang/Application/Admin/Controller/CommentController.class.php
+++ changxiang/Application/Admin/Controller/CommentController.class.php
@@ -2,7 +2,20 @@
 namespace Admin\Controller;
 use Think\Controller;
 class CommentController extends Controller {
+    //评论列表
     public function comment(){
+        //实例化comments模型对象
+        $commentsModel = M('comments');
+
         $this -> display();
     }
+    //添加评论
+    public function addcomment(){
+        $this -> display();
+    }
+    //编辑评论
+    public function editcomment(){
+        $this -> display();
+    }
+
 }
