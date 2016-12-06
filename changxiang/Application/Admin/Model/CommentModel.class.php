487e532c684812365a81bd0daa119567381bda90
liukaixin123 <3081766044@qq.com>
2016-12-06 11:23:15 +0800

    书评管理

--- changxiang/Application/Admin/Model/CommentModel.class.php
+++ changxiang/Application/Admin/Model/CommentModel.class.php
@@ -5,7 +5,7 @@ use Think\Model;
 
 class CommentModel extends Model{
     public $_validate =array(
-        //添加图书
+        //添加评论自定义模型
         array("commentuserid","require","发表评论用户的id不能为空"),
         array("bookreviewid","require","书评id不能为空"),
         array("commentcontent","require","评论内容不能为空"),
