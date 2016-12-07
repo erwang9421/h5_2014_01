1c4860818cbb8de55741d182e0535ed70d865176
Administrator <1820430115@qq.com>
2016-12-07 09:25:13 +0800

    点赞 修改

--- changxiang/Application/Admin/Controller/ZanController.class.php
+++ changxiang/Application/Admin/Controller/ZanController.class.php
@@ -1,12 +1,37 @@
 <?php
+
 namespace Admin\Controller;
 use Think\Controller;
-class ZanController extends Controller {
-	public function pushpraise(){
-		$this -> display();
-	}
 
+
+class ZanController extends Controller {
+
+    //编辑修改点赞信息
+	public function pushpraise(){
+		$ZanModel=D('Zan');
+		$Zan=$ZanModel->select();
+		$this->assign('Zan',$Zan);
+
+		$booksModel=D("Zan");
+		$id=$_GET['booksId'];
+		$Zan=$booksModel->find($id);
+		$this->assign('Zan',$Zan);
+		$this->display();
+	}
+    //添加点赞信息
     public function edit(){
-        $this -> display();
-    }
+        if (!IS_POST) {
+			exit("bad request请求");
+		}
+		$ZanModel=D("books");
+		if (!$ZanModel->create()) {
+			$this->error($ZanModel->getError());
+		}
+		if ($ZanModel->add()) {
+		    $this->success("添加成功",U("Zan/pushpraise"));
+		}else{
+			$this->error("添加失败");
+		}		
+	}
+    
 }
