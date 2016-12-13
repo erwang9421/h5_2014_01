81bc9f9e84290954de31db37df8d16c5ec4efdaa
liukaixin123 <3081766044@qq.com>
2016-12-13 11:08:06 +0800

    书评内容页浏览次数的实现

--- changxiang/Application/Home/Controller/BookController.class.php
+++ changxiang/Application/Home/Controller/BookController.class.php
@@ -2,7 +2,13 @@
 namespace Home\Controller;
 use Think\Controller;
 class BookController extends Controller {
-    public function bookreviewcontent(){
+    //index.html页面点击浏览全文，传入书评的id，显示书评详情页
+    public function bookreviewcontent($bookreviewid){
+        //书评浏览次数的实现
+        $in = M("bookreview");
+        $output = $in->where(array('bookreviewid' => $bookreviewid))->setInc('viewtimes',1);
+        $info = $in -> find($bookreviewid);
+        $this -> assign('info',$info);
         $this->display();
     }
 }
