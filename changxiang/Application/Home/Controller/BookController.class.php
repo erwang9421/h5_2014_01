eb7f4ee0381b91c1d1f110f677c52c7540176bdc
fengyugd <2495097326@qq.com>
2016-12-14 08:37:20 +0800

    home mine fy

--- changxiang/Application/Home/Controller/BookController.class.php
+++ changxiang/Application/Home/Controller/BookController.class.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace Home\Controller;
 use Think\Controller;
+
 class BookController extends Controller {
     //index.html页面点击浏览全文，传入书评的id，显示书评详情页
     public function bookreviewcontent($bookreviewid){
@@ -9,6 +11,11 @@ class BookController extends Controller {
         $output = $in->where(array('bookreviewid' => $bookreviewid))->setInc('viewtimes',1);
         $info = $in -> find($bookreviewid);
         $this -> assign('info',$info);
+
+        //显示书评标题以及书评内容
+        $bookReviewModel = M('bookreview');
+        $result = $bookReviewModel -> find($bookreviewid);
+        $this -> assign("bookReview",$result);
         $this->display();
     }
 }
