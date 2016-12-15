97ec82313e45ec8500040d30dedd357727bdd5c0
wangning <815664738@qq.com>
2016-12-15 11:29:34 +0800

    动态获取前台发表书评

--- changxiang/Application/Home/Controller/BookController.class.php
+++ changxiang/Application/Home/Controller/BookController.class.php
@@ -18,4 +18,41 @@ class BookController extends Controller {
         $this -> assign("bookReview",$result);
         $this->display();
     }
+
+
+     public function editarticle(){
+            // $this->show("前台默认");
+            $bookscategoriesModel = M("bookscategories");
+            $bookscategories=$bookscategoriesModel->select();
+
+            $bookreviewModel = M('bookreview');
+            $booksModel = M('books');
+            $list = $bookreviewModel->join('books ON books.bookid = bookreview.bookid')->select();
+
+            $this -> assign('bookscategories',$bookscategories);//赋值数据集
+            $this -> display();//输出模板
+        }
+        public function unlogindex(){
+            $this->display();
+        }
+
+
+        public function publish(){
+            if (!IS_POST) {
+                exit("bad request请求");
+            }
+            $booksModel=D("books");
+            if (!$booksModel->create()) {
+                $this->error($booksModel->getError());
+            }
+            if ($booksModel->add()) {
+                $this->success("添加成功..",U("Index/index"));
+            }else{
+                $this->error("添加失败");
+            }
+
+
+
+        }
+
 }
